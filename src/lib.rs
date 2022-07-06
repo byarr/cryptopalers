5171442b0e33d09d1fb254ce66473ecd67788621
Brian Yarr <brian.yarr@deliveroo.co.uk>
2022-07-06 19:53:12 +0100

    repeating_key_xor

--- src/lib.rs
+++ src/lib.rs
@@ -19,6 +19,14 @@ pub fn single_byte_xor(input: &[u8], key: u8) -> Vec<u8> {
     input.iter().map(|b| b ^ key).collect()
 }
 
+pub fn repeating_key_xor(input: &[u8], key: &[u8]) -> Vec<u8> {
+    input
+        .iter()
+        .zip(key.iter().cycle())
+        .map(|(b, k)| b ^ k)
+        .collect()
+}
+
 pub fn guess_single_byte_xor(input: &[u8]) -> Vec<ScoredDecrypt> {
     let scorer = scoring::ChiSquaredScorer {};
 
@@ -98,5 +106,18 @@ mod tests {
         let detected = detect_single_byte_xor(&cipher_texts);
 
         assert_eq!(detected[0].0, "Now that the party is jumping\n");
+    }
+
+    #[test]
+    fn test_repeating_key_xor() {
+        let in1 = "Burning 'em, if you ain't quick and nimble\nI go crazy when I hear a cymbal";
+        let key = "ICE";
+
+        let expected_hex = "0b3637272a2b2e63622c2e69692a23693a2a3c6324202d623d63343c2a26226324272765272a282b2f20430a652e2c652a3124333a653e2b2027630c692b20283165286326302e27282f";
+
+        assert_eq!(
+            repeating_key_xor(in1.as_bytes(), key.as_bytes()),
+            hex::decode(expected_hex).unwrap()
+        );
     }
 }
