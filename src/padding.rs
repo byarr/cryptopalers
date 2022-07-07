77ee9b5efb284d7a1e50b97ed3b1533f611a4ab2
Brian Yarr <brian.yarr@deliveroo.co.uk>
2022-07-07 14:09:16 +0100

    pkcs padding

---
+++ src/padding.rs
@@ -0,0 +1,29 @@
+fn calc_padding(data: &[u8], block_size: usize) -> usize {
+    block_size-(data.len() %  block_size)
+}
+
+fn pad(data: &mut Vec<u8>, block_size: usize) {
+    let padding = calc_padding(&data, block_size) as u8;
+    for i in 0..padding {
+        data.push(padding);
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_calc_padding() {
+        assert_eq!(4, calc_padding("YELLOW SUBMARINE".as_bytes(), 20));
+        assert_eq!(20, calc_padding("YELLOW SUBMARINE1234".as_bytes(), 20));
+    }
+
+    #[test]
+    fn test_pad() {
+        let mut data = Vec::from("YELLOW_SUBMARINE".as_bytes());
+        pad(&mut data, 20);
+
+        assert_eq!(data, Vec::from("YELLOW_SUBMARINE\x04\x04\x04\x04".as_bytes()));
+    }
+}
