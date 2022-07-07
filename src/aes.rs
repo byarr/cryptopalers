ab6123a0ba1c0e78884734408eee931e4a10f0c9
Brian Yarr <brian.yarr@deliveroo.co.uk>
2022-07-07 10:46:40 +0100

    detect_aes_128_ecb

--- src/aes.rs
+++ src/aes.rs
@@ -1,21 +1,48 @@
-use openssl::error::ErrorStack;
-use openssl::symm::{Cipher, Crypter, decrypt, Mode};
+use openssl::symm::{decrypt, Cipher};
+use std::collections::HashMap;
 
 pub fn aes_128_ecb(key: &[u8], input: &[u8]) -> Vec<u8> {
     let t = Cipher::aes_128_ecb();
-    let plain = decrypt(
-        t,
-        key,
-        None,
-        input).unwrap();
+    
 
-    plain
+    decrypt(t, key, None, input).unwrap()
+}
+
+pub fn detect_aes_128_ecb(possible_cipher_texts: Vec<Vec<u8>>) -> (usize, i32) {
+    fn hash_block(block: &[u8]) -> u128 {
+        let mut b_hash: u128 = 0;
+        block.iter().for_each(|b| {
+            b_hash <<= 8;
+            b_hash |= *b as u128;
+        });
+        b_hash
+    }
+
+    let block_size_bytes = 16;
+
+    possible_cipher_texts
+        .iter()
+        .enumerate()
+        .filter_map(|(idx, cipher_text)| {
+            let num_blocks = cipher_text.len() / block_size_bytes;
+
+            let counts = (0..num_blocks)
+                .map(|b| hash_block(&cipher_text[b * block_size_bytes..(b + 1) * block_size_bytes]))
+                .fold(HashMap::new(), |mut acc, i| {
+                    let entry = acc.entry(i).or_insert(0);
+                    *entry += 1;
+                    acc
+                });
+
+            counts.values().max().map(|m| (idx, *m))
+        })
+        .max_by(|b1, b2| b1.1.cmp(&b2.1))
+        .unwrap()
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
-    use std::fs::File;
     use std::io;
     use std::io::BufRead;
 
@@ -33,7 +60,19 @@ mod tests {
 
         let plain = String::from_utf8(plain_text).unwrap();
 
-        println!("{}", plain);
         assert!(plain.starts_with("I'm back and I'm ringin' the bell "));
+    }
+
+    #[test]
+    fn test_detect_aes_128_ecb() {
+        let data: Vec<_> =
+            io::BufReader::new(std::fs::File::open("./challenge-data/8.txt").unwrap())
+                .lines()
+                .filter_map(|r| r.ok())
+                .map(|s| hex::decode(s).unwrap())
+                .collect();
+
+        let idx = detect_aes_128_ecb(data);
+        assert_eq!(idx.1, 4);
     }
 }
