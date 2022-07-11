f924ae64228e32521d57d79c67b8db39369b6717
Brian Yarr <brian.yarr@deliveroo.co.uk>
2022-07-11 08:45:39 +0100

    validate_padding

--- src/block/padding.rs
+++ src/block/padding.rs
@@ -31,6 +31,29 @@ pub fn strip_padding(data: &mut Vec<u8>, block_size: usize) {
     }
 }
 
+pub fn validate_padding(data: &[u8], block_size: usize) -> Result<&[u8], ()> {
+    if data.len() % block_size != 0 {
+        return Err(());
+    }
+
+    let padding = *data.last().unwrap();
+    if padding as usize > block_size {
+        return Err(());
+    }
+
+    if !data
+        .iter()
+        .rev()
+        .take(padding as usize)
+        .cloned()
+        .all(|p| p == padding)
+    {
+        return Err(())
+    }
+
+    return Ok(&data[0..data.len()-padding as usize])
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -50,5 +73,17 @@ mod tests {
             data,
             Vec::from("YELLOW_SUBMARINE\x04\x04\x04\x04".as_bytes())
         );
+    }
+
+    #[test]
+    fn test_validate_padding() {
+        let valid = "ICE ICE BABY\x04\x04\x04\x04";
+        let expected = "ICE ICE BABY";
+
+        assert_eq!(validate_padding(valid.as_bytes(), 16), Ok(expected.as_bytes()));
+
+        for invalid in ["ICE ICE BABY\x05\x05\x05\x05", "ICE ICE BABY\x01\x02\x03\x04"] {
+            assert!(validate_padding(invalid.as_bytes(), 16).is_err());
+        }
     }
 }
